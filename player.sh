fb26eb1017321dfc076711bbd0c1b88a3ac69ec6
Markus Bach <markus@derpaderborner.de>
2012-04-12 19:37:49 +0200

    player komponente steht

---
+++ player.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# This software was written by Markus Bach <markus@derpaderborner.de>
+# It is licensed as beerware. So drink up and get me a beer!
+# Just kidding. Have fun with this piece of crap.
+
+# Kommandos werden in die inputdatei geschrieben, z.B. mit echo oder cat oder
+# was für Blödsinn du benutzen willst.
+# Momentan gibt es 2 Kommandos:
+# play [file] - Versucht die Datei file mit mplayer auszugeben
+# text [text] - Erzeugt ein Bild mit dem text und zeigt dieses an
+
+inputfile=input
+log=log
+background=alles_ist_gut.png
+textbild=$background
+dir=$PWD
+b=textbild.jpg
+
+pid=0
+play() {
+    mplayer $1 &
+    sleep 2s;
+    test $pid -eq 0 || kill $pid; pid=0
+}
+text() {
+    rm $b
+    width=$(identify -format %w $textbild)
+    convert -background '#000' -fill white -gravity center -pointsize 25\
+    -size ${width}x150 caption:"$1" $textbild +swap -gravity south\
+    -composite $b
+    feh -F "$b" &
+    pid=$!
+}
+main() {
+    feh -F $background &
+    while true; do
+        while read n; do
+            cmd=$(expr "$n" : '\([a-Z]*\)')
+            arg=${n##$cmd}
+            case "$cmd" in
+                play)
+                    file=$(basename $arg)
+                    test -f "$dir/$file" && play "$dir/$file" || echo "$(date) Datei '$file' nicht gefunden!">>$log
+                    ;;
+                text)
+                    text "$arg"
+                    ;;
+            esac
+        done < $inputfile
+    done
+    echo "Die schleife ist beendet"
+    killall feh
+}
+main
